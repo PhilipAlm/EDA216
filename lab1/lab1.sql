7ee79fcede7991cac2e5a6420e98370ce23b9411
Johan Davidsson <johan.et.davidsson@gmail.com>
2016-01-25 12:30:30 +0100

    add answers

--- lab1/lab1.sql
+++ lab1/lab1.sql
@@ -34,3 +34,34 @@ FROM courses
 WHERE coursecode LIKE 'FMA%';
 
 -- g)
+
+SELECT *
+FROM courses 
+WHERE credits > 7.5;
+
+-- h)
+
+SELECT count(*) 
+FROM courses
+WHERE level = 'A'; -- same for 'G2', 'G1'
+
+-- i)
+
+SELECT coursecode
+FROM takencourses
+WHERE pNbr = '910101-1234';
+
+-- j)
+
+SELECT coursename,credits
+FROM courses
+JOIN takencourses on (takencourses.coursecode = courses.coursecode)
+WHERE takencourses.pnbr = '910101-1234';
+
+
+
+
+
+
+
+
